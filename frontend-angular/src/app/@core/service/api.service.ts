b3b089c149fdcab6e78e8e15ba5604a8afaa340f
HhMorgan <heshammse@gmail.com>
2018-05-17 20:21:36 -0700

    Responsivity fix in Schedule, and Profile pages. Navbar responsivity fix

--- frontend-angular/src/app/@core/service/api.service.ts
+++ frontend-angular/src/app/@core/service/api.service.ts
@@ -12,7 +12,7 @@ import { APIData , Tag , Session , Request , CandicateSession , Profile , User ,
 @Injectable()
 export class APIService {
   public static apiUrl = 'https://whatwhynot.net/api/';
-  // public static apiUrl = 'http://127.0.0.1:3000/api/';
+  //public static apiUrl = 'http://127.0.0.1:3000/api/';
   public static apiUrl_Intercept_Ignore_list: Array<String> = [ 'auth/login' , 'auth/signup' , 'auth/verify' , 'auth/forgetPassword' ];
   constructor( private jwtHelper: JwtHelper , private http: HttpClient) { }
 
